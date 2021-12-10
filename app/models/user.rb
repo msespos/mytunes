4a373438bfbc90f4c9400603d8926bce9face058
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-10 07:42:47 -0800

    Generate Friendship model and add associations between User and User through Friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :followers, foreign_key: :follower_id, class_name: 'Friendship'
+  has_many :followed, through :followers
+  has_many :followed, foreign_key: :followed_id, class_name: 'Friendship'
+  has_many :followers, through :followed
 end
