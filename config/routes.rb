3ae9f1028e363f5f5e1f43b486f2c3ccb74aa4d4
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-12 09:16:29 -0800

    Generate Friendships controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'friendships/create'
+  get 'friendships/destroy'
   get 'users/index'
   devise_for :users
   devise_scope :user do
