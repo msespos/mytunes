5de593f13386adc020f9f91c71dcbd947c1de3bf
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-12 07:33:45 -0800

    Remove unnecessary migration

--- db/migrate/20211212132354_remove_request_confirmed_from_friendship_requests.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRequestConfirmedFromFriendshipRequests < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :friend_requests, :request_confirmed
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_140223) do
+ActiveRecord::Schema.define(version: 2021_12_10_235617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
