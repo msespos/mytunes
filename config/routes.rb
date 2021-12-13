7b1bd4c3a53133f172a3bd9d928832c365617d5f
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-12 17:39:48 -0800

    Edit create route and remove destroy path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'friendships/create'
-  get 'friendships/destroy'
   get 'users/index'
   devise_for :users
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
+  resources :friendships, only: [:create]
 end
