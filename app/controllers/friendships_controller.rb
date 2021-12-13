cc28765afdb4b27782b6b95021c3287f944305cf
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-12 17:39:01 -0800

    Add create action and friend_request_params

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,18 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friend_request = FriendRequest.new(friend_request_params)
+    if @friend_request.save
+      redirect_to users_index_path
+      flash[:notice] = "Friend Request Created!"
+    else
+      flash.now[:error] = "Could not save client"
+      render action: "new"
+    end
+  end
+
+  private
+
+  def friend_request_params
+    params.permit(:requesting_user_id, :requested_user_id)
+  end
 end
