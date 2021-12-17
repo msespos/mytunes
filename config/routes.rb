26852f5fd775dffa0ea94d81ff9affbc6c06bcc4
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-17 08:49:52 -0800

    Add create_friend_request route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@
 
 Rails.application.routes.draw do
   get 'users/index'
+
   devise_for :users
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
-  resources :friendships, only: [:create]
+
+  resources :friendships do
+    collection do
+      post 'create_friend_request'
+    end
+  end
 end
