1c6833592031457720b65c72f66a9f5346f5ca91
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-18 05:17:55 -0800

    Add to do comments

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,19 +3,24 @@ class FriendshipsController < ApplicationController
     @friendship = Friendship.new(friendship_params)
     if @friendship.save
       redirect_to users_index_path
+      # IS CALLING DESTROY_FRIEND_REQUEST HERE A GOOD IDEA???
       destroy_friend_request
       flash[:notice] = "Friendship created!"
     else
       flash.now[:error] = "Could not create friendship"
+      # POSSIBLY NOT RENDERING CORRECTLY - POSSIBLE NOMETHODERROR
+      # NEED TO CHECK THIS - HOW?
       render "users/index"
     end
   end
 
   def create_friend_request
     @friend_request = FriendRequest.new(friend_request_params)
+    # DOES THIS NEED TO BE DONE WITH "?"s?
     @duplicate_request = FriendRequest.all.where(
       requesting_user_id: friend_request_params[:requesting_user_id],
       requested_user_id: friend_request_params[:requested_user_id])
+    # DOES THIS NEED TO BE DONE WITH "?"s?
     @opposite_request = FriendRequest.all.where(
       requesting_user_id: friend_request_params[:requested_user_id],
       requested_user_id: friend_request_params[:requesting_user_id])
@@ -30,11 +35,15 @@ class FriendshipsController < ApplicationController
       flash[:notice] = "Friend request created!"
     else
       flash.now[:error] = "Could not create request"
+      # POSSIBLY NOT RENDERING CORRECTLY - POSSIBLE NOMETHODERROR
+      # NEED TO CHECK THIS - HOW?
       render "users/index"
     end
   end
 
   def destroy_friend_request
+    # DOES THIS NEED TO BE DONE WITH "?"s?
+    # ALSO IS IT A PROBLEM THAT USER1 AND USER2 HAVE SPECIFIC ROLES NOW?
     @friend_request = FriendRequest.all.where(
       requesting_user_id: friendship_params[:user1_id],
       requested_user_id: friendship_params[:user2_id])
