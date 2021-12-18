fb2d03d3b3c3abca49941fca9f98561758b757dd
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-18 07:13:30 -0800

    Add to do comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 # Users Controller class
 class UsersController < ApplicationController
   def index
+    # IS CREATING @USERS AND @FRIEND_REQUESTS IN TWO DIFFERENT CONTROLLERS OK?
     @users = User.all
     @friend_requests = FriendRequest.where('requested_user_id = ?', current_user.id)
   end
