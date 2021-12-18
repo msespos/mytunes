ccfd86ba595f1912ae6520b0d7edcf4f052e546c
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-18 07:12:37 -0800

    Add index action

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,7 @@
 class FriendRequestsController < ApplicationController
+  def index
+    # IS CREATING @USERS AND @FRIEND_REQUESTS IN TWO DIFFERENT CONTROLLERS OK?
+    @users = User.all
+    @friend_requests = FriendRequest.where('requested_user_id = ?', current_user.id)
+  end
 end
