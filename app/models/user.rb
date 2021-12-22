f70bedeac0abf495ad922f07ea5ee488513f57dd
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-22 12:05:27 -0800

    Add dont_show and pending_friends and edit friends to debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,32 @@ class User < ApplicationRecord
     Friendship.where('requested_user_id = ? AND confirmed = ?', id, false)
   end
 
+  def self.dont_show(current_user, u)
+    current_user.friends.include?(u) ||
+      u.pending_friends.include?(current_user) ||
+      current_user.pending_friends.include?(u)
+  end
+
+  def pending_friends
+    pending_friends = []
+    requesting_user_friends = Friendship.where('requested_user_id = ?
+                                                AND confirmed = ?', id, false)
+    requesting_user_friends.each do |f|
+      pending_friends << User.find(f.requesting_user_id)
+    end
+    pending_friends
+  end
+
   # NEED TO REFACTOR THIS?
   def friends
     friends = []
-    requesting_user_friends = Friendship.where('requested_user_id = ?', id)
+    requesting_user_friends = Friendship.where('requested_user_id = ?
+                                                AND confirmed = ?', id, true)
     requesting_user_friends.each do |f|
       friends << User.find(f.requesting_user_id)
     end
-    requested_user_friends = Friendship.where('requesting_user_id = ?', id)
+    requested_user_friends = Friendship.where('requesting_user_id = ?
+                                               AND confirmed = ?', id, true)
     requested_user_friends.each do |f|
       friends << User.find(f.requested_user_id)
     end
