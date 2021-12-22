f0dc43cc5540ca9157498fab11c99f93d5d2c07b
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-21 19:10:02 -0800

    Remove comments and destroy_friend_request and create_friend_request and add destroy action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,64 +11,22 @@ class FriendshipsController < ApplicationController
     @friendship = Friendship.new(friendship_params)
     if @friendship.save
       redirect_to users_index_path
-      # IS CALLING DESTROY_FRIEND_REQUEST HERE A GOOD IDEA???
-      # destroy_friend_request
-      flash[:notice] = 'Friendship created!'
+      flash[:notice] = 'You made a friendship request!'
     else
-      flash.now[:error] = 'Could not create friendship'
-      # POSSIBLY NOT RENDERING CORRECTLY - POSSIBLE NOMETHODERROR
-      # NEED TO CHECK THIS - HOW?
+      flash.now[:error] = 'Error - could not process friend request'
+      # NEED TO TEST THAT IS RENDERING CORRECTLY WHEN ERROR
       render 'users/index'
     end
   end
 
-  # SHOULD SOME OF THIS BE MADE MODEL CODE?
-  def create_friend_request
-    @friend_request = FriendRequest.new(friend_request_params)
-    # DOES THIS NEED TO BE DONE WITH "?"s?
-    @duplicate_request = FriendRequest.all.where(
-      requesting_user_id: friend_request_params[:requesting_user_id],
-      requested_user_id: friend_request_params[:requested_user_id]
-    )
-    # DOES THIS NEED TO BE DONE WITH "?"s?
-    @opposite_request = FriendRequest.all.where(
-      requesting_user_id: friend_request_params[:requested_user_id],
-      requested_user_id: friend_request_params[:requesting_user_id]
-    )
-    if @duplicate_request.first
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    if @friendship.destroy
       redirect_to users_index_path
-      flash[:notice] = "You've already made a friend request to them!"
-    elsif @opposite_request.first
-      redirect_to users_index_path
-      flash[:notice] = "They've already made a friend request to you!"
-    elsif @friend_request.save
-      redirect_to users_index_path
-      flash[:notice] = 'Friend request created!'
+      flash[:notice] = 'You denied the friendship request!'
     else
-      flash.now[:error] = 'Could not create request'
-      # POSSIBLY NOT RENDERING CORRECTLY - POSSIBLE NOMETHODERROR
-      # NEED TO CHECK THIS - HOW?
-      render 'users/index'
-    end
-  end
-
-  def destroy_friend_request
-    # DOES THIS NEED TO BE DONE WITH "?"s?
-    # ALSO WHAT ABOUT USING FRIENDSHIP_PARAMS TO DEAL WITH FRIEND_REQUEST?
-    # ALSO WhAT ABOUT GRABBING FIRST FRIEND_REQUEST?
-    @friend_request = FriendRequest.all.where(
-      requesting_user_id: friendship_params[:requesting_user_id],
-      requested_user_id: friendship_params[:requested_user_id]
-    )
-    Rails.logger.debug "@FRIEND_REQUEST: #{@friend_request}"
-    id = @friend_request.first.id
-    if @friend_request.destroy(id)
-      redirect_to users_index_path
-      flash[:notice] = 'Friend request denied!'
-    else
-      flash.now[:error] = 'Could not deny request'
-      # POSSIBLY NOT RENDERING CORRECTLY - POSSIBLE NOMETHODERROR
-      # NEED TO CHECK THIS - HOW?
+      flash.now[:error] = 'Error - could not process friend request'
+      # NEED TO TEST THAT IS RENDERING CORRECTLY WHEN ERROR
       render 'users/index'
     end
   end
@@ -79,9 +37,4 @@ class FriendshipsController < ApplicationController
     params.require(:friendship).permit(:requesting_user_id, :requested_user_id,
                                        :confirmed)
   end
-
-  #def friend_request_params
-  #  params.require(:friend_request).permit(:requesting_user_id,
-  #                                         :requested_user_id, :id)
-  #end
 end
