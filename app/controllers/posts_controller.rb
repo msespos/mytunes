c5f92ad8e18e586c842822603edc901472a1dce2
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-23 10:50:47 -0800

    Add new and create actions and post_params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    redirect_to posts_path
+    flash[:notice] = if @post.save
+                       'Post created!'
+                     else
+                       'Error - could not process post creation'
+                     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+  end
 end
