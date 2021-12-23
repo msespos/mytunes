2efe32419d9f244470185a5ee1adc825e0de3120
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-23 15:24:56 -0800

    Refactor logic from view into display_post? model method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
+
+  def display_post?(current_user)
+    self.user == current_user || current_user.friends.include?(self.user)
+  end
 end
