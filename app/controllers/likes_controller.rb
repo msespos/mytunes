1bc89734d86059c794fbb92cf4c46d8400ac10d0
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-24 07:13:30 -0800

    Add create action and like_params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.new(like_params)
+    redirect_to posts_path
+    flash[:notice] = if @like.save
+                       'You liked a post!'
+                     else
+                       'Error - could not process like'
+                     end
+  end
+
+  def like_params
+    params.require(:like).permit(:post_id, :user_id)
+  end
 end
