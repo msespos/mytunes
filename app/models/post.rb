0e76220760b3e8d71faa624a4c2ebc23f7e61fd6
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-24 07:14:07 -0800

    Add display_for? and can_be_liked_by? and number_of_likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,15 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
 
-  def display_post?(current_user)
+  def display_for?(current_user)
     self.user == current_user || current_user.friends.include?(self.user)
+  end
+
+  def can_be_liked_by?(current_user)
+    current_user.likes.where(post_id: self.id).empty?
+  end
+
+  def number_of_likes
+    Like.all.where(post_id: self.id).count
   end
 end
