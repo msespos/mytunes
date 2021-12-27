02e6ef808245ca9ec5a2422387aab7769742ad77
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-27 08:02:54 -0800

    Add gravatar_for helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@
 
 # Users Helper module
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=wavatar"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
