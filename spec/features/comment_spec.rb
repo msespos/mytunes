341d5ddcbdcadfba800d298cb32ede2883c34276
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-27 18:32:28 -0800

    Add tests for commenting

---
+++ spec/features/comment_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require 'rails_helper'
+
+RSpec.describe 'Comment on a post', type: :feature do
+  before(:all) do
+    @mike = create(:user)
+    @katie = create(:user, name: 'Katie', email: 'katie@example.com')
+  end
+
+  scenario 'log in and create a post and comment on that post' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    click_on 'New Post'
+    fill_in 'post[body]', with: 'My Post'
+    click_on 'Create Post'
+    expect(page).to have_content('Comment')
+  end
+
+  scenario 'log in and create a post and comment on that post' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    click_on 'New Post'
+    fill_in 'post[body]', with: 'My Post'
+    click_on 'Create Post'
+    fill_in 'comment[body]', with: 'My Comment'
+    click_on 'Create Comment'
+    expect(page).to have_content('My Comment')
+  end
+
+  scenario 'log in and create a post and comment on it and log in as a friend and comment on it' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    click_on 'New Post'
+    fill_in 'post[body]', with: 'My Post'
+    click_on 'Create Post'
+    fill_in 'comment[body]', with: 'My Second Comment'
+    click_on 'Create Comment'
+    click_on 'Back'
+    click_on 'Make'
+    click_on 'Sign out'
+    fill_in 'Email', with: 'katie@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    click_on 'Pending'
+    click_on 'Confirm'
+    click_on 'Home'
+    fill_in 'comment[body]', with: 'My Second Comment'
+    click_on 'Create Comment'
+    expect(page).to have_content('My Second Comment')
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
