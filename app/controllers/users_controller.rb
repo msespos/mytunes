36d077bb2db6251b517be8530e06bccd1caf3fc3
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-28 11:18:54 -0800

    Add edit and update and user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_index_path
+      flash[:notice] = 'You updated your profile!'
+    else
+      flash[:notice] = 'Error - could not process profile update'
+      render :edit                 
+    end 
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :project_name, :location,
+                                 :favorite_artist, :birthday)
+  end
 end
