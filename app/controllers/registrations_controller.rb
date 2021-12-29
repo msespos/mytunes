a5b14cc2d14adc9c0e5e93009fd564246aef1bd9
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-29 05:57:23 -0800

    Edit after sign up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
-    user_path(current_user.id)
+    edit_user_path(current_user.id)
   end
 end
