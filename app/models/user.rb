2ef1a94e021deacc89bbb24203ddb837efa4f36a
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-01 09:56:06 -0800

    Fix too-long line

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,8 @@ class User < ApplicationRecord
   end
 
   def friendships
-    Friendship.includes(:requesting_user).includes(:requested_user).where('(requested_user_id = ? OR requesting_user_id = ?)
+    Friendship.includes(:requesting_user).includes(:requested_user)
+              .where('(requested_user_id = ? OR requesting_user_id = ?)
                        AND confirmed = ?', id, id, true)
   end
 
