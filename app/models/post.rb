af4f440966af6b507cb5da7031a3b1197a09e5f9
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-04 10:34:26 -0800

    Move logic from can_be_liked_by? model method to view and delete model method

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,10 +10,6 @@ class Post < ApplicationRecord
     user == current_user || current_user.friends.include?(user)
   end
 
-  def can_be_liked_by?(current_user)
-    current_user.likes.where(post_id: id).empty?
-  end
-
   def number_of_likes
     Like.all.where(post_id: id).count
   end
