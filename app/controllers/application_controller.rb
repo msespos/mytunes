a35bd9bd8f4158eaa77056abda6af2bf28499d32
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-06 12:24:42 -0800

    Add username to Devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@
 
 # Application Controller class
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
   def after_sign_in_path_for(_resource)
     users_index_path
   end
