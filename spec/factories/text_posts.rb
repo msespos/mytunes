5f6788911db51adaaa9176de43fa941c9946704f
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-09 16:57:31 -0800

    Rename post to text_post in spec files

--- spec/factories/text_posts.rb
+++ spec/factories/text_posts.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :post do
+  factory :text_post do
     body { 'Merry Christmas!' }
   end
 end
--- spec/models/text_post_spec.rb
+++ spec/models/text_post_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe TextPost, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 end
