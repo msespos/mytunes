4cd1427a3e908776e3f169d9917c1d20ae519a86
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-13 07:46:01 -0800

    Add new and create actions and image_post_params

--- app/controllers/image_posts_controller.rb
+++ app/controllers/image_posts_controller.rb
@@ -1,2 +1,21 @@
 class ImagePostsController < ApplicationController
+  def new
+    @image_post = ImagePost.new
+  end
+
+  def create
+    @image_post = ImagePost.new(image_post_params)
+    @image_post.user_id = current_user.id
+    if @image_post.save
+      flash[:notice] = 'Post created!'
+      redirect_to image_posts_path
+    else
+      flash[:notice] = 'Error - could not process post creation'
+      render 'new'
+    end
+  end
+end
+
+def image_post_params
+  params.require(:image_post).permit(:image)
 end
