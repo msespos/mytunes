67bd55558eb89954b8be1ef2fce78902ffe69898
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-14 06:41:23 -0800

    move like and comment actions and like_params and comment_params from TextPosts controller to Posts controller

--- app/controllers/text_posts_controller.rb
+++ app/controllers/text_posts_controller.rb
@@ -18,41 +18,9 @@ class TextPostsController < ApplicationController
     end
   end
 
-  def like
-    @like = Like.new(like_params)
-    @like.user_id = current_user.id
-    @like.post = TextPost.find(like_params[:post_id])
-    flash[:notice] = if @like.save
-                       'You liked a post!'
-                     else
-                       'Error - could not process like'
-                     end
-    redirect_to posts_path
-  end
-
-  def comment
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.post = TextPost.find(comment_params[:post_id])
-    flash[:notice] = if @comment.save
-                       'You commented on a post!'
-                     else
-                       'Error - could not process comment'
-                     end
-    redirect_to posts_path
-  end
-
   private
 
   def text_post_params
     params.require(:text_post).permit(:body)
-  end
-
-  def like_params
-    params.require(:like).permit(:post_id)
-  end
-
-  def comment_params
-    params.require(:comment).permit(:post_id, :body)
   end
 end
