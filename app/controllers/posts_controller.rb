9ba1011daad9f40c745cf1c5629a84c152102ce5
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-14 06:08:37 -0800

    Edit @viewable_posts to include both text and image posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @viewable_posts = TextPost.order('created_at DESC').select do |p|
+    posts = TextPost.all + ImagePost.all
+    @viewable_posts = posts.sort { |x,y| y.created_at <=> x.created_at }.select do |p|
       p.viewable_by?(current_user)
     end
   end
