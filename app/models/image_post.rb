3be750286c27b0094a36d561f87638e6ec905672
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-14 06:09:27 -0800

    Add viewable_by? and number_of_likes to image_post model

--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -5,4 +5,12 @@ class ImagePost < ApplicationRecord
   has_many :likes, as: :post
   has_many :comments, as: :post
   has_one_attached :image
+
+  def viewable_by?(current_user)
+    user == current_user || current_user.friends.include?(user)
+  end
+
+  def number_of_likes
+    Like.all.where(post_id: id).count
+  end
 end
