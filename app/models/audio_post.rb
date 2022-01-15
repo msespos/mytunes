3c210443f924e85ac7fad70d650cde7dffad7db1
Mike Esposito <mikeesposito6852@gmail.com>
2022-01-15 07:56:17 -0800

    Add associations and model methods to AudioPost

--- app/models/audio_post.rb
+++ app/models/audio_post.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class AudioPost < ApplicationRecord
+  belongs_to :user
+  has_many :likes, as: :post
+  has_many :comments, as: :post
+  has_one_attached :sound
+
+  def viewable_by?(current_user)
+    user == current_user || current_user.friends.include?(user)
+  end
+
+  def number_of_audio_likes
+    Like.all.where(post_id: id, post_type: 'AudioPost').count
+  end
 end
