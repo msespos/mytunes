c22006babdb17fe7ea944961d1c1d979ec7170df
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-10 09:35:58 -0800

    Remove unnecessary sign up params override

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,8 +5,4 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(_resource)
     edit_user_path(current_user.id)
   end
-
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
 end
