fc69469de16441f26ba714eeb873ae51d2d5102f
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-10 09:35:30 -0800

    Remove unnecessary account update customization and add current_password to sign up customization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password,
+                                                               :password_confirmation) }
   end
 
   def after_sign_in_path_for(_resource)
