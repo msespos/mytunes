2260a486453010a8a32352435c8a3a2dfffe1875
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-10 10:13:57 -0800

    Style edit by Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password,
-                                                               :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password,
+               :password_confirmation)
+    end
   end
 
   def after_sign_in_path_for(_resource)
