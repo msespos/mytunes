2a7bf68eb611fb28ed699552ac2a052cc4d4eff4
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-17 07:46:47 -0800

    Rewrite tests to pass again after styling overhaul

--- spec/features/like_spec.rb
+++ spec/features/like_spec.rb
@@ -4,54 +4,43 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Like a post', type: :feature do
+RSpec.describe 'Liking a post', type: :feature do
   before(:all) do
     @mike = create(:user)
     @katie = create(:user, name: 'Katie', email: 'katie@example.com')
   end
 
-  scenario 'log in and create a post and like that post' do
+  scenario 'successful log in and post creation and post liking' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'New Text Post'
+    click_on 'New Post'
     fill_in 'text_post[body]', with: 'My Post'
-    click_on 'Create Text post'
-    expect(page).to have_content('Like')
-  end
-
-  scenario 'log in and create a post and like that post' do
-    visit root_path
-    fill_in 'Email', with: 'mike@example.com'
-    fill_in 'Password', with: '123456'
-    click_on 'Log in'
-    click_on 'New Text Post'
-    fill_in 'text_post[body]', with: 'My Post'
-    click_on 'Create Text post'
-    click_on 'Like'
+    click_on 'Submit'
+    click_on 'Like this post'
     expect(page).to have_content('1 Like')
   end
 
-  scenario 'log in and create a post and like it and log in as a friend and like it' do
+  scenario 'successful log in and post creation and post liking as two different users' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'New Text Post'
+    click_on 'New Post'
     fill_in 'text_post[body]', with: 'My Post'
-    click_on 'Create Text post'
-    click_on 'Like'
+    click_on 'Submit'
+    click_on 'Like this post'
     click_on 'Everybody'
-    click_on 'Make'
+    click_on '+Friend'
     click_on 'Sign out'
     fill_in 'Email', with: 'katie@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Pending'
+    click_on 'Everybody'
     click_on 'Confirm'
-    click_on 'My Feed'
-    click_on 'Like'
+    first(:xpath, "//a[contains(@href, '/posts')]").click
+    click_on 'Like this post'
     expect(page).to have_content('2 Likes')
   end
 end
