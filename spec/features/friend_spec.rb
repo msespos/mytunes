e4617bdfa7621eda0d26b71379391514f04483ea
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-17 07:22:10 -0800

    Rewrite tests to pass again after styling overhaul

--- spec/features/friend_spec.rb
+++ spec/features/friend_spec.rb
@@ -4,103 +4,92 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Create a friendship', type: :feature do
+RSpec.describe 'Creating a friendship', type: :feature do
   before(:all) do
     @mike = create(:user)
     @katie = create(:user, name: 'Katie', email: 'katie@example.com')
   end
 
-  scenario 'log in' do
+  scenario 'successful log in' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    expect(page).to have_content('Logged in as m:ke')
+    expect(page).to have_content('m:ke')
   end
 
-  scenario 'log in and request a friendship' do
+  scenario 'successful log in and friendship request' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Make Friend Request'
+    click_on 'Everybody'
+    click_on '+Friend'
     expect(page).to have_content('You made a friendship request!')
   end
 
-  scenario 'log in and request a friendship, log in as other user and confirm request' do
+  scenario 'successful log in and friendship request and confirmation as other user' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Make Friend Request'
+    click_on 'Everybody'
+    click_on '+Friend'
     click_on 'Sign out'
     fill_in 'Email', with: 'katie@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Pending Friend Requests'
+    click_on 'Everybody'
     click_on 'Confirm'
     expect(page).to have_content('You confirmed the friendship!')
   end
 
-  scenario 'log in and request a friendship, log in as other user and confirm request' do
+  scenario 'successful log in and friendship request and confirmation as other user' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Make Friend Request'
+    click_on 'Everybody'
+    click_on '+Friend'
     click_on 'Sign out'
     fill_in 'Email', with: 'katie@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Pending Friend Requests'
+    click_on 'Everybody'
     click_on 'Confirm'
-    expect(page).to_not have_content('Make Friend Request')
+    expect(page).to_not have_content('+Friend')
   end
 
-  scenario 'log in and request a friendship, log in as other user and deny request' do
+  scenario 'successful log in and friendship request and denial as other user' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Make Friend Request'
+    click_on 'Everybody'
+    click_on '+Friend'
     click_on 'Sign out'
     fill_in 'Email', with: 'katie@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Pending Friend Requests'
+    click_on 'Everybody'
     click_on 'Deny'
     expect(page).to have_content('You denied the friendship request!')
   end
 
-  scenario 'log in and request a friendship, log in as other user and deny request' do
+  scenario 'successful log in and friendship creation as other user' do
     visit root_path
     fill_in 'Email', with: 'mike@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Make Friend Request'
+    click_on 'Everybody'
+    click_on '+Friend'
     click_on 'Sign out'
     fill_in 'Email', with: 'katie@example.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
-    click_on 'Pending Friend Requests'
-    click_on 'Deny'
-    expect(page).to have_content('No Pending Friend Requests')
-  end
-
-  scenario 'log in, create a friendship, log in as other user and confirm, view' do
-    visit root_path
-    fill_in 'Email', with: 'mike@example.com'
-    fill_in 'Password', with: '123456'
-    click_on 'Log in'
-    click_on 'Make Friend Request'
-    click_on 'Sign out'
-    fill_in 'Email', with: 'katie@example.com'
-    fill_in 'Password', with: '123456'
-    click_on 'Log in'
-    click_on 'Pending Friend Requests'
+    click_on 'Everybody'
     click_on 'Confirm'
-    click_on 'Friends'
-    expect(page).to have_content('m:ke')
+    expect(page).to have_content("Your Friends:\nm:ke")
   end
 end
 
