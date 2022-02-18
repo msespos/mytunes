8f53377a16ace10d2bd993f58448b6bfabf3b77d
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-17 17:25:03 -0800

    Add feature tests for login

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require 'rails_helper'
+
+RSpec.describe 'Logging in', type: :feature do
+  before(:all) do
+    @mike = create(:user)
+    @katie = create(:user, name: 'Katie', email: 'katie@example.com')
+  end
+
+  scenario 'failed login with no email or password displays signup page' do
+    visit root_path
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    click_on 'Log in'
+    expect(page).to have_content('Log in')
+  end
+
+  scenario 'failed login with no email or password displays correct flash' do
+    visit root_path
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'failed login with no email displays correct flash' do
+    visit root_path
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'failed login with no password displays correct flash' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: ''
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'failed login with incorrect email displays correct flash' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.co'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'failed login with incorrect password displays correct flash' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '12345'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  scenario 'successful login displays confirmation flash' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  scenario 'successful login displays user home page' do
+    visit root_path
+    fill_in 'Email', with: 'mike@example.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+    expect(page).to have_content('Your Feed')
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
