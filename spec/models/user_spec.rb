a595c6f70faa875af3cf94cdd6051e8c5967a386
Mike Esposito <mikeesposito6852@gmail.com>
2022-02-28 14:34:03 -0800

    Add 3 model tests for User

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:all) do
+    @mike = User.create(name: "m:ke", email: "mike@example.com", password: "123456", id: 1)
+    @katie = User.create(name: "katie", email: "katie@example.com", password: "123456", id: 2)
+  end
+  describe '#request_friendship_with' do
+    context 'when a user requests a friendship with another user' do
+      it 'creates the friendship' do
+        saved = @mike.request_friendship_with({ requested_user_id: @katie.id })
+        expect(saved).to eq(true)
+      end
+    end
+  end
+  describe '#request_friendship_with' do
+    context 'when a user requests a friendship with another user' do
+      it 'creates the friendship with confirmed set to false' do
+        @mike.request_friendship_with({ requested_user_id: @katie.id })
+        friendship = Friendship.where(requesting_user_id: @mike.id).first
+        expect(friendship.confirmed).to eq(false)
+      end
+    end
+  end
+  describe '#request_friendship_with' do
+    context 'when a user requests a friendship with themselves' do
+      it 'does not create the friendship' do
+        saved = @mike.request_friendship_with({ requested_user_id: @mike.id })
+        expect(saved).not_to eq(true)
+      end
+    end
+  end
+end
+
+# rubocop:enable Metrics/BlockLength
